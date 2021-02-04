43385239472dd6f3f89e465664b90c2b2f28e9dc
S <ranjithkumar.s@dxc.com>
2021-02-04 11:23:33 +0530

    config changed

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/vbimusic'));
+// app.use(express.static(__dirname + '/vbimusic'));
+app.use(express.static(__dirname + '/dist/vbimusic'));
+
 
 
 // Serve only the static files form the dist directory
 // app.use(express.static('./dist/vbimusic'));
 
 app.get('/*', (req, res) =>
-res.sendFile(path.join(__dirname, 'vbimusic', 'index.html'))
+    res.sendFile(path.join(__dirname, '/dist/vbimusic/index.html'))
 
+    // res.sendFile(path.join(__dirname, 'vbimusic', 'index.html'))
     // res.sendFile('index.html', {root: 'dist/vbimusic/'}),
 );
 
